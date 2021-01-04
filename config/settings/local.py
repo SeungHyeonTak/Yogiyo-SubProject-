942eff46ba1a89808628b6520e982abb53794ad6
탁승현 <conficker77@gmail.com>
2021-01-05 00:22:06 +0900

    - django debug toolbar 설치

--- config/settings/local.py
+++ config/settings/local.py
@@ -3,6 +3,16 @@ from .base import *
 DEBUG = True
 ALLOWED_HOSTS = ['127.0.0.1']
 
+INSTALLED_APPS += [
+    'debug_toolbar',
+]
+
+MIDDLEWARE += [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+INTERNAL_IPS = ['127.0.0.1']
+
 # Local Postgresql
 DATABASES = {
     'default': {
--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -7,3 +8,9 @@ urlpatterns = [
     path('', include('app.urls', namespace='app')),
     path('', include('account.urls', namespace='account')),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ]
