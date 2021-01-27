7f36e5dc74b34f35e3c2d15dcd4b41a231807ccf
탁승현 <conficker77@gmail.com>
2021-01-27 23:57:40 +0900

    - owner site 로그인 페이지 구현

--- owner_site/urls.py
+++ owner_site/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from owner_site.views import owner_index
+from owner_site.views import owner_index, owner_login
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('', owner_index, name='owner'),
+    path('owner/login/', owner_login, name='owner_login'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
