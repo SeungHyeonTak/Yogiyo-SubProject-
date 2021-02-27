def2340d3d0eb7c04d13d2c1d072843532e34e8f
탁승현 <conficker77@gmail.com>
2021-02-27 22:16:15 +0900

    - ApplicationFormAdmin search_fields 추가

--- app/admin.py
+++ app/admin.py
@@ -18,7 +18,8 @@ class PaymentTypeAdmin(admin.ModelAdmin):
 
 @admin.register(ApplicationForm)
 class ApplicationFormAdmin(admin.ModelAdmin):
-    list_display = ('id', 'category', 'business_name',)
+    list_display = ('id', 'category', 'business_name', 'license_number',)
+    search_fields = ('business_name', 'license_number',)
     list_display_links = ('business_name',)
     list_filter = ('business_name',)
 
